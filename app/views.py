b4b5f2e3fa4251c0b5ef1b4dbf47cc14092a795e
Marc-Mwangi <marc.mwangi@student.moringaschool.com>
2022-02-11 11:09:26 +0300

    Setting up configuration

--- app/views.py
+++ app/views.py
@@ -3,6 +3,14 @@ from app import app
 
 #Views
 @app.route('/')
-def index():
+def dashboard():
 
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def sigup():
+    return render_template('signup.html')
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-from .app import app
+#!/usr/bin/env python3.8
+
+from app import app
 
 if __name__ == '__main__':
     app.run(debug = True)
