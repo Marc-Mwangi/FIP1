6431bccfe7d4818c51f7e815db89848b6a2f3307
Marc-Mwangi <marc.mwangi@student.moringaschool.com>
2022-02-11 11:11:36 +0300

    COmpleting configuration

---
+++ app/config.py
@@ -0,0 +1,14 @@
+class Config:
+    """
+    General configuraion
+    """
+    pass
+class Production(Config):
+    """
+    Production configuration
+    """
+    pass
+class Development(Config):
+    """
+    Development configuration
+    """
